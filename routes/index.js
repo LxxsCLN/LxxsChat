5edd26822c34a4701695c2c3ce92b3509a8d0515
LxxsCLN <113736900+LxxsCLN@users.noreply.github.com>
2023-07-06 22:51:46 -0700

    added view for new msg, displays messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hola!",
+    user: "Luis",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Lxxs Chat", messages: messages })
 });
+
+router.get("/new", function(req,res,next){
+  res.render("form", {title: "Nuevo Mensaje"})
+})
+
+router.post("/new", function(req,res,next){
+  const newobj = {
+    text: req.body.messagetext,
+    user: req.body.messageauthor,
+    added: new Date()
+  }
+  messages.push(newobj)
+  res.redirect('/');
+})
 
 module.exports = router;
